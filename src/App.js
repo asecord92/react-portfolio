9a0d5c408d72cbc0527aa3525c7ae2bd31c8baf0
The Stretch <ats92@cox.net>
2021-12-20 18:12:26 -1000

    basic About me section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import Nav from './components/Nav'
-
+import About from './components/About'
 
 function App() {
   return (
     <>
     <Nav/>
     <main>
-      
+    <About/>
     </main>
     </>
   );
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container, Typography} from '@mui/material';
+
+function About() {
+    return (
+        <Container maxWidth='md'>
+            <Typography variant='h6' align='center' paragraph>
+            Hello! My name is Adam Secord, and this is a compilation of all the work I am (mostly) proud of! I started my journey into coding only recently, but it something that I have always found so intriguing. Prior to this endevour, I have spent most of my professional career working in the healthcare field, ranging from being a first responder up to what I am currently doing, which has to do with coordinating healthcare-based research intiatives. So as you can tell, trying to jump into a career in coding is quite the change, but a very welcomed one. My goal is to gather as much knowledge as possible, and to achieve my goal of becoming a web developer! Outside of work, I love to both play and watch sports. My time away from my computer is typically spent watching some of my favorite teams, golfing or just being outside.
+
+            </Typography>
+        </Container>
+    )
+}
+
+export default About;
